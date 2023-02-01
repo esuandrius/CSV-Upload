1d509f0bc7975a95a4fcd64ae91e88467454000c
esuandrius.github.io <111871226+esuandrius@users.noreply.github.com>
2023-02-01 11:15:51 +0200

    Delete backend/app directory

--- backend/app/src/main/java/com/csv/upload/app/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csv.upload.app.repository;
-
-import com.csv.upload.app.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
